9980976747138dfdc56367036b52b87cd63705bb
Gustavo <gustavohiroaki@gmail.com>
2019-04-30 05:34:10 -0300

    new db

--- utl/homologacao.sql
+++ utl/homologacao.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 24-Abr-2019 às 22:26
--- Versão do servidor: 10.1.38-MariaDB
--- versão do PHP: 5.6.40
+-- Generation Time: 30-Abr-2019 às 10:33
+-- Versão do servidor: 10.1.36-MariaDB
+-- versão do PHP: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -84,7 +84,47 @@ INSERT INTO `events` (`id_event`, `name_event`, `date_init`, `hour_init`, `date_
 (24, 'Tesde de evento após git commit', '2019-04-01', '12:13:00', '2019-04-17', '14:14:00', '2019-04-01 12:13:00', '2019-04-17 14:14:00', 0, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento', 'anotacoes', 'Katahira', '1', '2019-04-15 03:38:49'),
 (25, 'Aniversário Argentina', '2019-04-19', '08:00:00', '2019-04-20', '20:00:00', '2019-04-19 08:00:00', '2019-04-20 20:00:00', 0, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Batata', 'N sei', 'Filial Argentina', '1', '2019-04-16 23:11:53'),
 (26, 'Los Angeles', '2019-04-24', '12:00:00', '2019-04-25', '14:00:00', '2019-04-24 12:00:00', '2019-04-25 14:00:00', 0, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento', 'Teste', 'N sei', '1', '2019-04-23 12:20:42'),
-(27, 'Evento teste numero membros', '2019-04-26', '14:44:00', '2019-04-27', '14:44:00', '2019-04-26 14:44:00', '2019-04-27 14:44:00', 1, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento', '', '', '1', '2019-04-24 18:58:11');
+(27, 'Evento teste numero membros', '2019-04-26', '14:44:00', '2019-04-27', '14:44:00', '2019-04-26 14:44:00', '2019-04-27 14:44:00', 1, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento', '', '', '1', '2019-04-24 18:58:11'),
+(28, 'Evento1', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:08:56'),
+(29, 'Evento2', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:02'),
+(30, 'Evento3', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:06'),
+(31, 'Evento4', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:12'),
+(32, 'Evento5', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:16'),
+(33, 'Evento6', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:20'),
+(34, 'Evento7', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:25'),
+(35, 'Evento8', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:30'),
+(36, 'Evento9', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:35'),
+(37, 'Evento10', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:40'),
+(38, 'Evento11', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:44'),
+(39, 'Evento12', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:48'),
+(40, 'Evento13', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:53'),
+(41, 'Evento14', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:09:57'),
+(42, 'Evento15', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:10:02'),
+(43, 'Evento16', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:10:06'),
+(44, 'Evento17', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:10:11'),
+(45, 'Evento18', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:10:15'),
+(46, 'Evento19', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:10:19'),
+(47, 'Evento20', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', '', '123123', '123123', '1', '2019-04-25 01:10:24'),
+(48, 'Evento1', '2019-04-01', '11:11:00', '2019-04-17', '22:22:00', '2019-04-01 11:11:00', '2019-04-17 22:22:00', 1414, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento Update', '123123', '123123', '1', '2019-04-27 00:34:00'),
+(49, 'Evento de Hoje', '2019-04-26', '21:43:00', '2019-04-26', '22:00:00', '2019-04-26 21:43:00', '2019-04-26 22:00:00', 20, '07190023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento', 'Anotacao', 'Eu', '1', '2019-04-27 00:46:01'),
+(50, 'Evento de Hoje', '2019-04-26', '21:43:00', '2019-04-26', '22:00:00', '2019-04-26 21:43:00', '2019-04-26 22:00:00', 20, '07190023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento', 'Anotacao', 'Eu', '2', '2019-04-27 00:47:03'),
+(51, 'Los Angeles', '2019-05-09', '12:00:00', '2019-05-15', '14:00:00', '2019-05-09 12:00:00', '2019-05-15 14:00:00', 30, '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Teste', 'Teste', 'Katahira', '1', '2019-04-29 03:29:39'),
+(52, 'Evento dia 2', '2019-05-02', '04:11:00', '2019-05-02', '11:11:00', '2019-05-02 04:11:00', '2019-05-02 11:11:00', 123, '07190023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Asdasdasd', 'asdadasda', 'sdasdasdasdasdas', '1', '2019-04-30 00:24:23'),
+(53, 'Evento dia 4', '2019-05-04', '04:11:00', '2019-05-04', '11:11:00', '2019-05-04 04:11:00', '2019-05-04 11:11:00', 123, '07190023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Asdasdasd', 'asdadasda', 'sdasdasdasdasdas', '1', '2019-04-30 00:24:37'),
+(54, '', '1970-01-01', '00:00:00', '1970-01-01', '00:00:00', '1970-01-01 00:00:00', '1970-01-01 00:00:00', 0, '', '', '', '', '', '', '', '', '', '2019-04-30 02:09:03'),
+(55, '', '1970-01-01', '00:00:00', '1970-01-01', '00:00:00', '1970-01-01 00:00:00', '1970-01-01 00:00:00', 0, '', '', '', '', '', '', '', '', '', '2019-04-30 02:11:14');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `event_branch`
+--
+
+CREATE TABLE `event_branch` (
+  `id_eventbranch` int(11) NOT NULL,
+  `id_event` int(11) NOT NULL,
+  `id_branch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -144,14 +184,15 @@ CREATE TABLE `members_rkmd` (
 --
 
 INSERT INTO `members_rkmd` (`id_member`, `name_member`, `username_member`, `id_type`, `pass_member`, `permission_member`, `id_branch`, `entrydate_member`, `birth_member`, `tel_member`, `rg_member`, `zip_member`, `state_member`, `city_member`, `neighboorhood_member`, `street_member`, `complement_member`, `status_member`, `log_member`) VALUES
-(1, 'Gustavo Hiroaki', 'gustavo.hiroaki', '3', 'gus2019@', 100, 1, '1970-01-01', '1970-01-01', '', '', '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento', '1', '2019-03-26 14:30:11'),
+(1, 'Gustavo Hiroaki Imafuku', 'gustavo.hiroaki', '3', 'gus2019@', 100, 2, '1970-01-01', '1970-01-01', '', '', '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento', '1', '2019-03-26 14:30:11'),
 (2, NULL, 'hideki.katahira', NULL, 'hide2019', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-03-27 17:31:13'),
 (5, 'Test1', '', '', '', 0, 0, '0000-00-00', '0000-00-00', '', '', '', '', '', '', NULL, '', '', '2019-04-01 11:45:46'),
 (6, '', '', '', '', 0, 0, '0000-00-00', '0000-00-00', '', '', '', '', '', '', NULL, '', '', '2019-04-01 12:58:04'),
 (7, 'Bruna Sueko Higa', 'bruna.sueko', '3', 'brubru', 1000, 1, '0000-00-00', '0000-00-00', '', '', '', '', '', '', NULL, '', '', '2019-04-04 18:50:57'),
 (8, 'Teste Cadastro', 'teste.cadastro', '3', '123456', 100, 1, '2019-04-03', '2019-04-03', '123456', '123456', '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento', '1', '2019-04-14 03:54:18'),
 (9, 'Angélica Nishiharo Kono', 'angelica.kono', '3', 'haichan00', 100, 1, '2017-10-25', '2000-08-13', '(11)97171-8424', '53.348.541-1', '07095-070', 'SP', 'Guarulhos', 'Jardim Zaira', 'Rua Doutor Miguel Vieira Ferreira 119', 'Apartamento', '1', '2019-04-16 23:10:33'),
-(10, 'Mario Kenzo A', 'mario.kassai', '1', '123123', 100, 1, '2019-04-02', '2019-04-02', '123123', '123123', '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento', '1', '2019-04-17 19:09:18');
+(10, 'Mario Kenzo A', 'mario.kassai', '3', '123123', 100, 1, '2019-04-02', '2019-04-02', '123123', '123123', '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'Complemento', '1', '2019-04-17 19:09:18'),
+(11, 'Solange Noriko Tanoue Imafuku', 'solange.noriko', '3', 'nori100', 200, 1, '2019-04-29', '2018-11-14', '111111', '11111', '07190-023', 'SP', 'Guarulhos', 'Parque Cecap', 'Rua Rubens Henrique Picchi', 'asasdasdasdas', '1', '2019-04-29 03:34:07');
 
 -- --------------------------------------------------------
 
@@ -186,6 +227,15 @@ CREATE TABLE `music` (
   `repertoire_music` varchar(1) DEFAULT NULL,
   `path_music` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `music`
+--
+
+INSERT INTO `music` (`id_music`, `name_music`, `singer_music`, `repertoire_music`, `path_music`) VALUES
+(1, 'Teste', NULL, NULL, NULL),
+(3, 'Sanshin no Hana.mp3', NULL, NULL, NULL),
+(4, 'Sanshin no Hana.mp3', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -240,6 +290,12 @@ ALTER TABLE `branch`
 --
 ALTER TABLE `events`
   ADD PRIMARY KEY (`id_event`);
+
+--
+-- Indexes for table `event_branch`
+--
+ALTER TABLE `event_branch`
+  ADD PRIMARY KEY (`id_eventbranch`);
 
 --
 -- Indexes for table `event_music`
@@ -302,7 +358,13 @@ ALTER TABLE `branch`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id_event` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id_event` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+
+--
+-- AUTO_INCREMENT for table `event_branch`
+--
+ALTER TABLE `event_branch`
+  MODIFY `id_eventbranch` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `event_music`
@@ -320,7 +382,7 @@ ALTER TABLE `members_music`
 -- AUTO_INCREMENT for table `members_rkmd`
 --
 ALTER TABLE `members_rkmd`
-  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `member_type`
@@ -332,7 +394,7 @@ ALTER TABLE `member_type`
 -- AUTO_INCREMENT for table `music`
 --
 ALTER TABLE `music`
-  MODIFY `id_music` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_music` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `notification`
