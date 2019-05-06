e2992518f7f21064cba414a3f14deac0070132b9
Gustavo Hiroaki <gustavo.hiroaki@csc.corp>
2019-05-06 17:14:59 -0300

    started prepare dashboard

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -3,11 +3,13 @@
 namespace app\controllers;
 
 use app\core\Controller;
-use app\models\Basics;
+// use app\models\Basics;
+use app\models\ConsultModel;
 
 class DashboardController extends Controller{
     public function index(){
         $this->security();
+        $query = new ConsultModel;
 
         $this->add_view("dashboard_main");
         $this->master_interface("dashboard");
