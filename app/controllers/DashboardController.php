67d47ca8e6d277de2c9d9a6b003a3b87c7fb9f40
Gustavo <gustavohiroaki@gmail.com>
2019-05-07 05:43:48 -0300

    added dashboard query

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -3,14 +3,15 @@
 namespace app\controllers;
 
 use app\core\Controller;
-// use app\models\Basics;
-use app\models\ConsultModel;
+
+use app\models\DashboardModel;
 
 class DashboardController extends Controller{
     public function index(){
         $this->security();
-        $query = new ConsultModel;
+        $query = new DashboardModel;
 
+        $this->
         $this->add_view("dashboard_main");
         $this->master_interface("dashboard");
 
@@ -18,3 +19,23 @@ class DashboardController extends Controller{
 
     
 }
+
+use homologacao;
+
+SELECT * FROM events 
+JOIN event_branch ON events.id_event = event_branch.id_event
+WHERE now() BETWEEN date_init AND date_fin
+AND id_branch = 1
+LIMIT 5;
+
+SELECT * FROM messages
+JOIN message_branch ON messages.id_message = message_branch.id_message
+WHERE now() BETWEEN date_init AND date_fin
+AND id_branch = 1
+LIMIT 5;
+
+SELECT * FROM next_training
+JOIN training_branch ON next_training.id_training = training_branch.id_training
+WHERE now() < date_training
+AND id_branch = 1
+LIMIT 5;
---
+++ app/models/DashboardModel.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models;
+
+use app\core\Model;
+
+class DashboardModel extends Model{
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function selectEvents(){
+        $sql = "
+            SELECT * FROM events 
+            JOIN event_branch ON events.id_event = event_branch.id_event
+            WHERE now() BETWEEN date_init AND date_fin
+            AND id_branch = 1
+            LIMIT 5
+        ";
+
+        $select = $this->db->query($sql);
+        return $select->fetchAll();
+    }
+
+    public function selectMessages(){
+        $sql = "
+            SELECT * FROM messages
+            JOIN message_branch ON messages.id_message = message_branch.id_message
+            WHERE now() BETWEEN date_init AND date_fin
+            AND id_branch = 1
+            LIMIT 5
+        ";
+
+        $select = $this->db->query($sql);
+        return $select->fetchAll();
+    }
+
+    public function selectTraining(){
+        $sql = "
+            SELECT * FROM next_training
+            JOIN training_branch ON next_training.id_training = training_branch.id_training
+            WHERE now() < date_training
+            AND id_branch = 1
+            LIMIT 5
+        ";
+
+        $select = $this->db->query($sql);
+        return $select->fetchAll();
+    }
+}
